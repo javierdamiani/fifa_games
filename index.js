349097596b54cded10d6e1f1b29be8be89f5a738
Frank345-sys <francgonzalez456@gmail.com>
2023-08-16 21:08:19 -0600

    Sección infor terminada

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import rankingYear from './ranking_year.js';
+import rankingYear from "./ranking_year.js";
 
-const chartContainer = document.querySelector('.ranking__container');
+const chartContainer = document.querySelector(".ranking__container");
 
 const uniqueYears = new Set();
 
@@ -9,24 +9,24 @@ rankingYear.forEach((data) => {
 });
 
 uniqueYears.forEach((year) => {
-  const yearCircle = document.createElement('div');
-  yearCircle.classList.add('ranking__circle');
+  const yearCircle = document.createElement("div");
+  yearCircle.classList.add("ranking__circle");
   yearCircle.dataset.year = year;
 
-  const yearText = document.createElement('span');
-  yearText.classList.add('ranking__circle-text');
+  const yearText = document.createElement("span");
+  yearText.classList.add("ranking__circle-text");
   yearText.textContent = year;
 
-  const rankedCountries = document.createElement('div');
-  rankedCountries.classList.add('ranking__circle-countries');
+  const rankedCountries = document.createElement("div");
+  rankedCountries.classList.add("ranking__circle-countries");
 
   const topCountries = rankingYear
     .filter((entry) => entry.year === year && entry.fifa_rank <= 3)
     .map((entry) => entry.team);
 
   topCountries.forEach((country) => {
-    const countryElement = document.createElement('div');
-    countryElement.classList.add('ranking__circle-country');
+    const countryElement = document.createElement("div");
+    countryElement.classList.add("ranking__circle-country");
     countryElement.textContent = country;
     rankedCountries.appendChild(countryElement);
   });
@@ -36,16 +36,39 @@ uniqueYears.forEach((year) => {
   chartContainer.appendChild(yearCircle);
 });
 
-const yearCircles = document.querySelectorAll('.ranking__circle');
+const yearCircles = document.querySelectorAll(".ranking__circle");
 
 yearCircles.forEach((circle) => {
-  circle.addEventListener('mouseover', () => {
-    const rankedCountries = circle.querySelector('.ranking__circle-countries');
-    rankedCountries.style.display = 'block';
+  circle.addEventListener("mouseover", () => {
+    const rankedCountries = circle.querySelector(".ranking__circle-countries");
+    rankedCountries.style.display = "block";
   });
 
-  circle.addEventListener('mouseout', () => {
-    const rankedCountries = circle.querySelector('.ranking__circle-countries');
-    rankedCountries.style.display = 'none';
+  circle.addEventListener("mouseout", () => {
+    const rankedCountries = circle.querySelector(".ranking__circle-countries");
+    rankedCountries.style.display = "none";
   });
 });
+
+//Este es un código para la sección info donde para la transición de imagenes, lo pongo aquí para no mover
+// si es necesario lo puedes mover.
+
+const backgroundDiv = document.querySelector(".info__img");
+const images = [
+  "./images/info.jpg",
+  "./images/info2.jpg",
+  "./images/info3.jpg",
+];
+let currentImageIndex = 0;
+
+function changeBackground() {
+  currentImageIndex = (currentImageIndex + 1) % images.length;
+  backgroundDiv.style.opacity = 0;
+
+  setTimeout(() => {
+    backgroundDiv.setAttribute("src", images[currentImageIndex]);
+    backgroundDiv.style.opacity = 1;
+  }, 1000);
+}
+
+setInterval(changeBackground, 5000);
